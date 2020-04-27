5133abfa5891cb103b5f7cc856ac875e95a005f4
mauradriscoll <51652231+mauradriscoll@users.noreply.github.com>
2020-04-27 13:17:32 -0400

    changes to main

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import sys
 import argparse
 from pycatfd import catfd_formain as catfd
-#from Face_Morph import face_morph as face_morph
-#from Detect_Human_Facial_Features import detect_face_features as humanfd
+#from Face_Morph import face_morph_main as face_morph
+from Detect_Human_Facial_Features import detect_human as humanfd
 
 def main():
     parser = argparse.ArgumentParser()
 
     #input images
     parser.add_argument("-c", "--cat", required=True, help="Enter filename of Cat")
-    #parser.add_argument("-h", "--human", required=True, help="Enter filename of Human")
+    parser.add_argument("-human", "--human", required=True, help="Enter filename of Human")
 
 
     args = parser.parse_args()
-    #print(args)
+    print(args)
     cat_image = args.cat
-    #print(cat_image)
-    #human_image = args.human
+    human_image = args.human
+    
 
     #Get cat features
-
     cat_features = catfd.detect(cat_image)
+    #Get human features
+    human_features = humanfd.detect(human_image)
     print(cat_features)
+    print(human_features)
+    #TODO: Get proper aligned features into file to test with face_morph
 
 if __name__ == '__main__':
     main()
